7f69fe9f2b219acfcd2abc0337454d4fedf48490
Vigitant <xaninsf@gmail.com>
2023-02-10 09:36:52 -0700

    fixed the shell script. website functioning now.

--- deployFiles.sh
+++ deployFiles.sh
@@ -2,14 +2,14 @@
 
 while getopts k:h:s: flag
 do
-    case "$[flag]" in
+    case "${flag}" in
         k) key=${OPTARG};;
         h) hostname=${OPTARG};;
         s) service=${OPTARG};;
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" || -z "$service"]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
     printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
@@ -17,11 +17,11 @@ fi
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
 
-#step 1
+# Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
-mdkir -p services/${service}/public
+mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
